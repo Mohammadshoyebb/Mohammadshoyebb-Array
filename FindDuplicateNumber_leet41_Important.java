ed103b15fe3ea7cb6e38c630be4dc0057971e5c5
Mohammadshoyebb <141633689+Mohammadshoyebb@users.noreply.github.com>
2024-03-30 16:58:06 +0530

    important leet41

--- FindDuplicateNumber_leet41_Important.java
+++ FindDuplicateNumber_leet41_Important.java
@@ -127,4 +127,23 @@ class FindDuplicateNumber_leet41_Important {
 // }
 
 
+//======================Another Method Using Hashset==============================
+// class Solution {
+//     public int findDuplicate(int[] nums) {
+//     HashSet<Integer> set = new HashSet<>(); 
+//     int ans =0;
+//     for(int i=0;i<nums.length;i++){
+       
+//         if(set.contains(nums[i])){
+//             ans =  nums[i];
+//             break;
+//         }
+
+//         else{
+//             set.add(nums[i]);
+//         }
+//     }return ans;
+// }
+// }
+
 
